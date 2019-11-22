3946c9aaf1b22711a3b73a747a2134d307accc03
thaweeph <thaweeph@kku.ac.th>
2019-11-22 09:08:21 +0700

    INIT

---
+++ src/ModalAjaxAsset.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TJY\widgets\modal;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class ModalAjaxAsset
+ * @package lo\widgets\modal
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class ModalAjaxAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public $js = [
+        'js/kb-modal-ajax.js'
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'css/modal-colors.css',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . "/assets";
+        parent::init();
+    }
+}
